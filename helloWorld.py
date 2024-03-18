87b7f6cc7742720c3cc7a959e52296d7b27ed0da
GLawani <163910115+GLawani@users.noreply.github.com>
2024-03-18 20:56:56 +0000

    Implement changes for meaningful issue-1

--- helloWorld.py
+++ helloWorld.py
@@ -3,3 +3,9 @@ print("Git is awesome!")
 user_input = input("Enter some data: ")
 print("You entered:", user_input)
 print("Git actions completed.")
+# Added a new function to calculate the factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
